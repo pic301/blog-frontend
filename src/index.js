9752de06b8f7f9f4b0f3b03cd1a0fea5813e937b
pic301 <ppic301@gmail.com>
2019-11-29 05:07:30 +0900

    add Sample redux

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
+   <Provider store={store}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
+   </Provider>
 , document.getElementById('root'));
 
