166ca0dceef4cf2d95abe4ce4ec4e0a6a9e5ebee
pic301 <ppic301@gmail.com>
2019-11-29 12:54:16 +0900

    add AuthForm

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled,{ css } from 'styled-components'
 import palette from '../../lib/styles/palette'
 
 const StyledButton = styled.button`
@@ -11,10 +11,28 @@ const StyledButton = styled.button`
     outline: none;
     cursor: pointer;
     
-    background: ${palette.gray[8]};
+    background: ${palette.gray[6]};
     &:hover{
-        background:${palette.gray[6]};
+        background:${palette.gray[5]};
     }
+    ${props =>
+    props.fullWidth &&
+    css`
+    padding-top: 0.75rem;
+    padding-bottom: 0.75rem;
+    width: 100%;
+    font-size: 1.125rem;
+    
+    `}
+
+    ${props =>
+    props.cyan &&
+    css`
+        background: ${palette.cyan[5]};
+        &:hover{
+            background:${palette.cyan[4]};
+        }
+    `}
 `;
 
 
