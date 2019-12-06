92e3eb38c7b689ef48295bcb43a82ef0833285b4
pic301 <ppic301@gmail.com>
2019-12-06 20:22:48 +0900

    add createRequestActionTypes

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,9 +1,19 @@
 import { createAction, handleActions } from 'redux-actions';
 import produce from 'immer';
+import createRequestSaga, {
+    createRequestActionTypes
+  } from '../lib/createRequestSaga'
 
 const CHANGE_FIELD = 'auth/CHANGE_FIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 
+const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] = createRequestActionTypes(
+    'auth/REGISTER'
+  );
+  
+const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] = createRequestActionTypes(
+    'auth/LOGIN'
+  );
 
 
 export const changeField = createAction(
